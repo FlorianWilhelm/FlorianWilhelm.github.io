42676779ef356bee3361b27c14373fe85dff05c8
Florian Wilhelm <florian.wilhelm@gmail.com>
2016-01-09 10:08:29 +0100

    Added README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+Blog of Florian Wilhelm
+=======================
+
+This is my blog hosted on http://www.florianwilhelm.info/. 
+Feel free to let me know about errors and typos.
+
--- pelican/generate_site.sh
+++ pelican/generate_site.sh
@@ -5,7 +5,7 @@ OUTPUT="$( readlink -f "$DIR/../" )"
 # Clean output directory
 cd $OUTPUT
 shopt -s extglob
-rm -rf !(pelican|_config.yml|CNAME)
+rm -rf !(pelican|_config.yml|CNAME|README.md)
 # Generate content
 cd $DIR
 pelican $DIR/content -o $OUTPUT

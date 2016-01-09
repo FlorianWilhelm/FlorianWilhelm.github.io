f5fb8d3c19684c18fab3922fbd471de4ebed0882
Florian Wilhelm <florian.wilhelm@gmail.com>
2016-01-09 10:31:33 +0100

    Added .nojekyll

--- pelican/generate_site.sh
+++ pelican/generate_site.sh
@@ -5,7 +5,7 @@ OUTPUT="$( readlink -f "$DIR/../" )"
 # Clean output directory
 cd $OUTPUT
 shopt -s extglob
-rm -rf !(pelican|_config.yml|CNAME|README.md)
+rm -rf !(pelican|_config.yml|CNAME|README.md|.nojekyll)
 # Generate content
 cd $DIR
 pelican $DIR/content -o $OUTPUT
